57feb36d7649e6765fbb6f4b3974c8c918290ca4
Divyajeet Singh <46.divyajeetsingh@gmail.com>
2020-07-28 21:12:37 +0530

    started js

---
+++ app.js
@@ -0,0 +1,13 @@
+//1) declare, 2) defining 
+
+// prompt("enter data") -> to take input
+// alert("show data") -> to show data on webpage.
+//to join to string -> string concetanation
+
+var a = 5;
+var b = "5";
+var c = a/b;
+ 
+// alert(c)
+console.log(c)
+console.log("hi how are you")
