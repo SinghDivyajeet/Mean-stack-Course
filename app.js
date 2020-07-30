7974372dee4376c995460291eca14df82ef53e77
Divyajeet Singh <46.divyajeetsingh@gmail.com>
2020-07-30 21:07:38 +0530

    started functions

--- app.js
+++ app.js
@@ -4,10 +4,71 @@
 // alert("show data") -> to show data on webpage.
 //to join to string -> string concetanation
 
-var a = 5;
-var b = "5";
-var c = a/b;
- 
-// alert(c)
-console.log(c)
-console.log("hi how are you")
+// operands and operators-> -,+,*,/,**, ++, -- , %
+//1) post-increament/decreament.
+//2) pre-increament/decreament.
+
+var a = 2;
+var b = 5;
+
+
+// console.log(typeof b)
+
+//number
+//string
+//object
+//null 
+//undefined
+//boolean -> true, false
+
+//Function naming rules:
+// 1)function keyword
+// 2) function name
+// 3) double round brakets
+// 4) cruly braces / paranthesis.
+// 5) the code to run will come between braces.
+
+//How to use functions:
+// 1) make a function.
+// 2) call the function.
+
+
+//Types of functions:
+// 1)non-returning, no-parameter 
+// 2)returning, no-parameter
+// 3)non-returning, parameter
+// 4)returning, parameter
+
+// parameter -> is something which you give to the function.
+// return ->  is what function returns you.
+function subtraction(){
+    //code
+    // type-1
+    var a = 6;
+    var b = 2;
+    var c = a-b;
+    console.log(c)
+}
+
+function multiply(){
+    //type-2
+    var a = 2;
+    var b = 4;
+    var c = a*b;
+    return c;
+}
+
+function addition(){
+    // type-1
+    var a = prompt("Enter 1st number:");
+    var b =  prompt("Enter 2nd number:");
+    var c = Number(a)+Number(b);
+    alert(c);
+}
+
+
+function sayFullName(name){
+    writeInEmail("Hi "+name);
+}
+
+sayFullName(name5);
