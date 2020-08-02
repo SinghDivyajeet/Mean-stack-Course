94145fe58c42d7fd1f9ec833eb215452ccfddcd3
Divyajeet Singh <46.divyajeetsingh@gmail.com>
2020-08-02 13:14:18 +0530

    learned nesting of objects

--- app.js
+++ app.js
@@ -68,7 +68,108 @@ function addition(){
 
 
 function sayFullName(name){
-    writeInEmail("Hi "+name);
+    console.log("Hi "+name);
 }
 
-sayFullName(name5);
+// sayFullName("name5");
+
+
+var mulBy2 = function (number,num2,...arguments){
+    console.log(...arguments)
+    return number*num2;
+}
+
+// console.log("10 Multiplies by 2 is "+mulBy2(1,2,3,4,5,6,7,8,9));
+
+var student1 = {
+    name : "Divyajeet",
+    age: 23,
+    marks : 100,
+    section: "A",
+    getInfo: function(){
+        console.log(student1.name+" has "+ student1.marks +", is in Section "+student1.section);
+    },
+    getAge: function(num){
+        return num*student1.age;
+    }
+}
+
+// console.log(student1.getAge(1)); 
+
+// JSON -> JavaScript Standard Object Notation.
+// XML -> Xtensible Markup language
+
+
+// console.log(student1["marks"],student1["name"],student1["age"]) //-> we can add data too.
+
+
+// console.log(student1.marks,student1.name,student1.age,student1.sport) //-> we can only read the Data.
+
+
+var companyDetails = {
+    name:"MRF",
+    emp: 1600,
+    getDetails: function(){
+        return `The name of company is ${companyDetails["name"]} and has ${companyDetails["emp"]} epms.`
+    },
+}
+
+console.log(companyDetails.getDetails());
+
+
+// How to manipulate Strings
+// 1) String mutation
+// "The name of company is "+companyDetials["name"]+" and has "+companyDetials["emp"]+" emps.";
+
+// 2) String interpolation
+// `static Part ${Dynamic part}`
+// `The name of company is ${companyDetails["name"] and has ${companyDetails["emp"]} epms.}`
+
+
+
+var bankAcc = {
+    HolderName: "Divyajeet",
+    AccNo:"456789",
+    AccType: "Saving",
+    Balance : "$45678",
+}
+
+var sport = {
+    sportName: "table tennis",
+    ranking: "A",
+    nextTournament: "Paris"
+}
+
+var drivesCar = {
+    carName: "Honda City",
+}
+
+var student = {
+    name: "Divyajeet Singh",
+    banking: {
+        HolderName: "Divyajeet",
+        AccNo:"456789",
+        AccType: "Saving",
+        Balance : "$45678",
+        retailBanking:{
+            credential:"67o",
+            accType:"shared",
+            sharedBtw: {
+                name: "rohini",
+                relation: "wife"
+            }
+        }
+    },
+    sport:{
+        sportName: "table tennis",
+        ranking: "A",
+        nextTournament: "Paris"
+    }
+}
+
+
+// console.log(student.drivesCar)
+
+// Array
+var num = ["rohan","mohan","rphan2","raghab","sohan",2,3,4,5,6,];
+console.log(num[0])
