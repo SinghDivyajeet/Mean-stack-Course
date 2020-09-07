1eed500c0751033a61d6ccfc4a97b6def5a7f80b
Divyajeet Singh <46.divyajeetsingh@gmail.com>
2020-09-07 21:07:41 +0530

    taught express routing

--- LearnExpress/app.js
+++ LearnExpress/app.js
@@ -1,11 +1,33 @@
+var express = require('express');
+
+var app = express();
+var port = 5000;
+
+//making routes.
+// static assests -> public -> styles, images and icons, etc.
+// pages -> views
+// use and set?
+//
+app.use(express.static("public")); // to specify to look out static assets in public folder.
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+  res.send("HI it's working with nodemon 123")
+})
+
+app.get("/dogs",(req,res)=>{
+  res.render("dogs")
+})
+
+app.get("/dogs/:breed",(req,res)=>{
+  var x = req.params.breed;
+  res.render("breed",{dogBreed:x});
+})
 
 
-//response code -> 1xx,2xx,3xx,4xx,5xx
-//java -> maven
-//python -> pipnp
-//ruby -> gem
-//node -> npm
-//semantic versioning
+app.listen(port, function(){
+    console.log('server running on '+ port);
+})
 
 
 
