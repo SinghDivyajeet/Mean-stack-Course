d3cc38a85ef48127c5dfc4d498644095d1dbeca1
Divyajeet Singh <46.divyajeetsingh@gmail.com>
2020-09-17 07:05:27 +0530

    taught about fetch api

--- LearnExpress/app.js
+++ LearnExpress/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fetch = require("node-fetch");
 const { response } = require('express');
 
 var app = express();
@@ -10,7 +11,18 @@ app.use(bodyParser.urlencoded({extended:true}))
 // static assests -> public -> styles, images and icons, etc.
 // pages -> views
 // use and set?
-//
+
+// Cross origin resource sharing -> CORS
+// Same-origin policy -> SOP
+// how to find usable data from api call. -> urlencoded, JSON.
+
+// SQL database -> Structured query language
+    // ACID Properties -> sole reason to use databases.
+
+// when to use SQL Dbs and NOSQL Dbs?
+
+//What is SQL.
+
 app.use(express.static("public")); // to specify to look out static assets in public folder.
 app.set("view engine","ejs"); // view engine is a engine which views/renders the page, you gotta specify which templating engine you are using.
 
@@ -47,6 +59,23 @@ app.post("/addRecipe",(req,res)=>{
             };
   recipes.push(obj);
   res.redirect("/recipes");
+})
+app.get("/github",(req,res)=>{
+  res.render("github");
+})
+app.get("/profile",(req,res)=>{
+  res.render("profile");
+})
+app.post("/search",(req,res)=>{
+  var name = req.body.user;
+  fetch("https://api.github.com/search/users?q=divyajeet")
+  .then(res=>{
+    console.log(res.body)
+  }).catch(err=>{
+    console.log(err)
+  })
+  
+  res.render("profile");
 })
 
 app.listen(port, function(){
