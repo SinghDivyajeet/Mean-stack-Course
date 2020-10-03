7847b47aec6f8c474fbd72a30d0ac0b8f4274b8f
Divyajeet Singh <46.divyajeetsingh@gmail.com>
2020-10-03 23:47:25 +0530

    taught axios and how to fetch data from API

--- LearnExpress/app.js
+++ LearnExpress/app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var fetch = require("node-fetch");
-const { response } = require('express');
+var axios = require('axios')
+
 
 var app = express();
-var port = 5000;
+var port =  5000;
 
 app.use(bodyParser.urlencoded({extended:true}))
 //making routes.
@@ -23,6 +23,16 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 //What is SQL.
 
+// CRUD application
+  // C -> create
+  // R -> Read
+  // U -> Update
+  // D -> Delete
+
+  // Two parts of setting connection to DB from backend.
+  //1. Connect DB
+  //2. how to recieve data and make it meaningfull on FE.
+
 app.use(express.static("public")); // to specify to look out static assets in public folder.
 app.set("view engine","ejs"); // view engine is a engine which views/renders the page, you gotta specify which templating engine you are using.
 
@@ -30,7 +40,7 @@ app.get("/",(req,res)=>{
   res.render("home")
 })
 
-var recipes = [{
+var recipesArray= [{
   name:"Burger",
   imgUrl:""
 },{
@@ -43,8 +53,10 @@ var recipes = [{
   name:"Mojito",
   imgUrl:""
 }];
+
+// var EmpData  = "select * from empDetails";
 app.get("/recipes",(req,res)=>{
-  res.render("recipes",{recipes:recipes});
+  res.render("recipes",{ValueToBePassed:recipesArray});
 })
 
 app.get("/dogs/:breed",(req,res)=>{
@@ -57,8 +69,8 @@ app.post("/addRecipe",(req,res)=>{
             name:req.body.recipeName,
             imgUrl:"",
             };
-  recipes.push(obj);
-  res.redirect("/recipes");
+            recipesArray.push(obj);
+  res.redirect("recipes");
 })
 app.get("/github",(req,res)=>{
   res.render("github");
@@ -66,18 +78,22 @@ app.get("/github",(req,res)=>{
 app.get("/profile",(req,res)=>{
   res.render("profile");
 })
-app.post("/search",(req,res)=>{
+app.post("/search",(req,response)=>{
   var name = req.body.user;
-  fetch("https://api.github.com/search/users?q=divyajeet")
-  .then(res=>{
-    console.log(res.body)
-  }).catch(err=>{
+  var url = `https://api.github.com/search/users?q=${name}`
+  axios.get(url).then((res)=>{
+    console.log(typeof(res.data.items))
+    response.render("profile",{data:res.data});
+  }).catch((err)=>{
     console.log(err)
+    response.render("error");
   })
   
-  res.render("profile");
+  
 })
 
+// {a:b} -> a = the variable which will be passed in ejs file to be used.
+      // -> b = is also a variable of the backend that holds data.
 app.listen(port, function(){
     console.log('server running on '+ port);
 })
